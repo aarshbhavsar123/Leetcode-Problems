65854112afd6186c4d19192bc3cff7a86b14dbea
Aarsh Bhavsar <97463622+aarshbhavsar123@users.noreply.github.com>
2024-09-17 08:46:00 +0530

    Add uncommon-words-from-two-sentences.cpp

---
+++ uncommon-words-from-two-sentences.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<string> uncommonFromSentences(string s1, string s2) {
+        unordered_set<string>st1,st2;
+        int n = s1.length();
+        int m = s2.length();
+        string s = "";
+        for(int i = 0;i<n;i++)
+        {
+            s+=s1[i];
+            
+        }
+    }
+};
